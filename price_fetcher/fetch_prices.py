782d8165a4eaba4a224f1c6e266a949098038e99
zach <zdicklin@gmail.com>
2024-07-23 22:43:10 -0600

    Price fetcher

--- price_fetcher/fetch_prices.py
+++ price_fetcher/fetch_prices.py
@@ -1,34 +1,46 @@
-# price_fetcher/fetch_prices.py
+# TODO:
+# - Save to DB
+# - Run every 10 seconds
+
+import logging
 import os
-import time
-import requests
-import psycopg2
-from datetime import datetime
+import sys
+from coinbaseadvanced.client import CoinbaseAdvancedTradeAPIClient
+from dotenv import load_dotenv
 
-DATABASE_URL = os.getenv('DATABASE_URL')
+load_dotenv()
 
-def fetch_and_store_price():
-    try:
-        response = requests.get('https://api.coinbase.com/v2/prices/spot?currency=USD')
-        data = response.json()
-        price = data['data']['amount']
-        timestamp = datetime.utcnow()
+coinbase_client = None
+logger = logging.getLogger(__name__)
 
-        conn = psycopg2.connect(DATABASE_URL)
-        cursor = conn.cursor()
-        cursor.execute(
-            "INSERT INTO prices (timestamp, price) VALUES (%s, %s)",
-            (timestamp, price)
-        )
-        conn.commit()
-        cursor.close()
-        conn.close()
+print(os.getenv("COINBASE_API_PRIVATE_KEY").replace("\\n", "\n"))
 
-    except Exception as e:
-        print(f"Error fetching or storing price: {e}")
 
-if __name__ == "__main__":
-    while True:
-        fetch_and_store_price()
-        time.sleep(60)  # Fetch every minute
+try:
+    coinbase_client = CoinbaseAdvancedTradeAPIClient.from_cloud_api_keys(
+        os.getenv("COINBASE_API_KEYNAME"),
+        os.getenv("COINBASE_API_PRIVATE_KEY").replace("\\n", "\n"),
+    )
+except Exception as e:
+    logger.error(f"Failed to initialize the Coinbase Advanced Trade API Client: {e}")
+    sys.exit(1)
 
+products = coinbase_client.list_products()
+product_ids = list(map(lambda p: p.product_id, products.products))
+
+# Filter product list to strings that end with "-USDC", like "BTC-USDC"
+product_ids = list(filter(lambda p: p.endswith("-USDC"), product_ids))
+
+best_bids_asks = coinbase_client.get_best_bid_ask(product_ids)
+
+
+def pricebook_to_dict(pricebook):
+    return {
+        "bid": pricebook.bids[0].price,
+        "ask": pricebook.asks[0].price,
+        "product_id": pricebook.product_id,
+    }
+
+
+pricebook_dict = list(map(lambda p: pricebook_to_dict(p), best_bids_asks.pricebooks))
+print(pricebook_dict)
